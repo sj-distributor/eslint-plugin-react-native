d319d600fb4f623c5d24aceff63608758124fcd4
Simon <simoonf@vip.qq.com>
2022-10-28 17:15:28 +0800

    style: Eslint format

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,18 +6,23 @@ module.exports = {
     es2022: true,
   },
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
+  plugins: ["unicorn"],
   extends: [
     "eslint:recommended",
     "plugin:eslint-plugin/recommended",
     "plugin:node/recommended",
+    "plugin:prettier/recommended",
   ],
   rules: {
-    "node/no-unpublished-require": ["error", {
-      "allowModules": ["requireindex"]
-    }]
+    "node/no-unpublished-require": [
+      "error",
+      {
+        allowModules: ["requireindex"],
+      },
+    ],
   },
   overrides: [
     {
--- configs/.eslintrc.js
+++ configs/.eslintrc.js
@@ -1,32 +1,27 @@
 module.exports = {
+  root: true,
   extends: [
     "eslint:recommended",
     "plugin:prettier/recommended",
     "plugin:react-hooks/recommended",
-    "plugin:@typescript-eslint/recommended"
+    "plugin:@typescript-eslint/recommended",
   ],
   parser: "@typescript-eslint/parser",
-  plugins: [
-    "react",
-    "react-native",
-    "simple-import-sort",
-    "unicorn",
-    "import"
-  ],
+  plugins: ["react", "react-native", "simple-import-sort", "unicorn", "import"],
   env: {
-    "es2022": true,
-    "browser": true,
-    "react-native/react-native": true
+    es2022: true,
+    browser: true,
+    "react-native/react-native": true,
   },
   parserOptions: {
-    "sourceType": "module",
-    "ecmaVersion": "latest",
-    "ecmaFeatures": {
-      "jsx": true
-    }
+    sourceType: "module",
+    ecmaVersion: "latest",
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
   rules: {
-    "camelcase": "error",
+    camelcase: "error",
     "spaced-comment": "error",
     "import/extensions": "off",
     "import/no-unresolved": "off",
@@ -38,40 +33,41 @@ module.exports = {
     "unicorn/filename-case": [
       "error",
       {
-        "cases": {
-          "kebabCase": true,
-          "camelCase": false,
-          "snakeCase": false,
-          "pascalCase": false
-        }
-      }
+        cases: {
+          kebabCase: true,
+          camelCase: false,
+          snakeCase: false,
+          pascalCase: false,
+        },
+      },
     ],
     "react-hooks/exhaustive-deps": [
       "error",
       {
-        "additionalHooks": "(useMyCustomHook|useMyOtherCustomHook|useRecoilCallback)"
-      }
+        additionalHooks:
+          "(useMyCustomHook|useMyOtherCustomHook|useRecoilCallback)",
+      },
     ],
     "padding-line-between-statements": [
       "error",
-      { "blankLine": "always", "prev": "*", "next": "return" }, // return 语句之前要有空行
+      { blankLine: "always", prev: "*", next: "return" }, // return 语句之前要有空行
       {
-        "blankLine": "always",
-        "prev": ["function", "const", "var", "let"], // 指定语法糖语句之间要有空行
-        "next": "*"
-      }
-    ]
+        blankLine: "always",
+        prev: ["function", "const", "var", "let"], // 指定语法糖语句之间要有空行
+        next: "*",
+      },
+    ],
   },
   // 共享设置，会提供给每一个将被执行的规则
   settings: {
     "import/resolver": {
-      "node": {
-        "extensions": [".js", ".jsx", ".ts", ".tsx"]
-      }
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
     },
-    "react": {
+    react: {
       // ‘detect’ 会自动选择你所安装的 React 版本
-      "version": "detect"
-    }
-  }
-}
+      version: "detect",
+    },
+  },
+};
