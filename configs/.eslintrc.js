9c656ca7e5063018a0ded682b9a24af20cef39c2
Simon <simoonf@vip.qq.com>
2022-10-28 18:16:48 +0800

    chore: Supplement eslint rules notes

--- configs/.eslintrc.js
+++ configs/.eslintrc.js
@@ -21,36 +21,39 @@ module.exports = {
     },
   },
   rules: {
-    camelcase: "error",
-    "spaced-comment": "error",
-    "import/extensions": "off",
-    "import/no-unresolved": "off",
-    "no-duplicate-imports": "error",
-    "react/react-in-jsx-scope": "off",
-    "react-hooks/rules-of-hooks": "error",
-    "simple-import-sort/imports": "error",
-    "simple-import-sort/exports": "error",
+    camelcase: "error", // 强制要求变量命名使用驼峰式
+    "spaced-comment": "error", // 将强制执行注释 // 或 /* 开始后的间距的一致性
+    "import/extensions": 0, // 关闭解析为模块并检查 exports 的文件扩展名列表，默认为.js 如果是React 或者使用 TypeScript 讲要去配置共享配置 settings
+    "import/no-unresolved": 0, // 关闭确保导入的模块可以解析为本地文件系统上的模块
+    "no-duplicate-imports": "error", // 每个模块只能有一个 imports，相同的内容可以在各自的 imports 上看到导入的内容
+    "react/react-in-jsx-scope": 0, // 关闭使用 jsx 时不允许缺少 React 的引入
+    "simple-import-sort/imports": "error", // imports 排序
+    "simple-import-sort/exports": "error", // exports 排序
+    // 强制文件名大小写样式
     "unicorn/filename-case": [
       "error",
       {
         cases: {
-          kebabCase: true,
-          camelCase: false,
-          snakeCase: false,
-          pascalCase: false,
+          kebabCase: true, // 是否支持横杠 (-) 命名
+          camelCase: false, // 是否支持小驼峰命名
+          snakeCase: false, // 是否支持 (_) 下划线命名
+          pascalCase: false, // 是否支持大坨峰命名
         },
       },
     ],
+    // Hooks 有依赖项的话强制要求补充依赖项
+    "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": [
       "error",
       {
         additionalHooks:
-          "(useMyCustomHook|useMyOtherCustomHook|useRecoilCallback)",
+          "(useMyCustomHook|useMyOtherCustomHook|useRecoilCallback)", // additionalHooks: 验证自定义的 Hooks 依赖
       },
     ],
+    // 要求或不允许在给定的2种语句之间有空行
     "padding-line-between-statements": [
       "error",
-      { blankLine: "always", prev: "*", next: "return" }, // return 语句之前要有空行
+      { blankLine: "always", prev: "*", next: "return" }, //  要求 return 语句之前要有空行
       {
         blankLine: "always",
         prev: ["function", "const", "var", "let"], // 指定语法糖语句之间要有空行
@@ -58,7 +61,7 @@ module.exports = {
       },
     ],
   },
-  // 共享设置，会提供给每一个将被执行的规则
+  // 共享配置，提供给每一个将被执行的规则
   settings: {
     "import/resolver": {
       node: {
@@ -66,8 +69,7 @@ module.exports = {
       },
     },
     react: {
-      // ‘detect’ 会自动选择你所安装的 React 版本
-      version: "detect",
+      version: "detect", // ‘detect’ 会自动选择你所安装的 React 版本
     },
   },
 };
